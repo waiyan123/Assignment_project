cdf4d3994c0e9ba3c05edb690be57e7f9d3b2cae
waiyan123 <itachialpha@gmail.com>
2023-01-17 13:40:05 +0630

    fixed text overlapping in detail screen

--- app/src/main/java/com/tunity/assignment/ui/util/UiConstants.java
+++ app/src/main/java/com/tunity/assignment/ui/util/UiConstants.java
@@ -8,13 +8,6 @@ public class UiConstants {
 
     //default values
     public static final String author = "Author - ";
-    public static final String default_author = "Anonymous";
-    public static final String default_title = "Untitled";
-    public static final String default_description = "No description";
     public static final String url = "Read more details... ";
-    public static final String default_url = "No source";
-    public static final String default_url_to_image = "";
     public static final String publish_at = "Published at - ";
-    public static final String default_publish_at = "Unknown";
-    public static final String default_content = "No content available for this.";
 }
